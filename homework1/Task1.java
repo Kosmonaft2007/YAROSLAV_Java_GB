7d28bc5983c34ce0b3eb052aaabf1ec8e4c027f7
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-11-23 11:00:28 +0300

    hot fix #1 Task1

--- homework1/Task1.java
+++ homework1/Task1.java
@@ -5,17 +5,17 @@ public class Task1{
 	
 	// треугольное число
 	static int triangle_sum(int num){
-		if (num == 1 || num == 0){
+		if (num == 1){
 			return 1;
 		}
 		return triangle_sum(num - 1) + num;
 	}
 	// произведение чисел
 	static int factorial(int num){
-		if (num == 1 || num == 0){
+		if (num == 1){
 			return 1;
 		}
-		return triangle_sum(num - 1) * num;
+		return factorial(num - 1) * num;
 	}
 	
     public static void main(String args[]){
