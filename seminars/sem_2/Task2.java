51a875b1091911cb7fc9587749dd86d34d9adc1c
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-11-28 22:25:19 +0300

    homework2, all tasks

---
+++ seminars/sem_2/Task2.java
@@ -0,0 +1,26 @@
+package sem_2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String str = "aaaabbbcdd";
+        System.out.println(compress(str));
+    }
+
+    private static String compress(String str){
+        int count = 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+
+                builder.append((str.charAt(i - 1)));
+                builder.append(count);
+                count = 1;
+            }
+        }
+        builder.append((str.charAt(str.length() - 1)));
+        return builder.toString();
+    }
+}
+
