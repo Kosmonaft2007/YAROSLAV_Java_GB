850c414f4d6598452f6d1ec14bfe1181f776dd94
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2022-12-01 10:07:13 +0300

    Update Task3.java

--- homeworks/homework3/Task3.java
+++ homeworks/homework3/Task3.java
@@ -25,13 +25,16 @@ public class Task3 {
         List<Integer> list = randomNumber(20, 1000);
         System.out.printf("Исходный список: %s\n", list);
 
-        // находим значения минимум, максимум и среднее арифметическое с помощью Stream API
-        // чтобы определить значения минимум, максимум нужно передать в параметры потока переопредленный метод compare, класса-обертки Integer
+        /* 
+	находим значения минимум, максимум и среднее арифметическое с помощью Stream API
+        Чтобы определить значения минимум, максимум нужно вызвать терминальный метод min/max и 
+	передать в параметры потока переопредленный метод compare, класса-обертки Integer 
+	*/
         Optional<Integer> max = list.stream().max(Integer::compare);
         Optional<Integer> min = list.stream().min(Integer::compare);
 		
         // преобразуем поток в IntStream с помощью mapToInt, вызываем метод average, 
-	 // который возвращает объект типа OptionalDouble, проебразуем его в Double методом getAsDouble.
+	// который возвращает объект типа OptionalDouble, проебразуем его в Double методом getAsDouble.
         Double avg = list.stream().mapToInt(i -> i).average().getAsDouble();
 		
         System.out.printf("Минимальное значение: %s\n", min.get());
