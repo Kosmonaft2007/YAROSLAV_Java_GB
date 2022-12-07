62f0493ca6b20e8275ca83beea16cb0dc5090f7b
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-12-07 22:45:18 +0300

    update #2

--- homeworks/homework5/Task1.java
+++ homeworks/homework5/Task1.java
@@ -1,49 +1,53 @@
+package homework5;
+
+
 import java.util.*;
-import java.util.regex.*; 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 Реализуйте структуру телефонной книги с помощью HashMap, 
 учитывая, что один человек может иметь несколько телефонов
 */
-public class Task1{
-	
-	//метод добавляет запись в телефонную книгу
-	private static Map<String, ArrayList<String>> addPersonToPhoneBook(String name, String phone) {
-		Map<String, ArrayList<String>> phoneBook = new HashMap();
-		
-	}
-	
-	/*
-	метод использует регулярное выражение для проверки 
-	правильности ввода номера телефона
-	*/
-	private static boolean isPhoneNumber(String phone) {
-		Pattern pat = Pattern.compile("^(7|8){1}[9]{1}[0-9]{9}");
-		Matcher mat = pat.matcher(phone);
-		return mat.find();
-	}
+public class Task1 {
+
+    /*
+    метод, использует регулярное выражение для проверки
+    правильности ввода номера телефона
+    */
+    private static boolean isPhoneNumber(String phone) {
+        Pattern pat = Pattern.compile("^([8]){1}[9]{1}[0-9]{9}");
+        Matcher mat = pat.matcher(phone);
+        return mat.find();
+    }
 
 
-    public static void main(String args[]){
-	Map<String, LinkedList<String>> phoneBook = new HashMap();
-	
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter name: ");
-	String name = scan.nextLine();
-	
-	while(!name.equals("quit") {
-		System.out.println("Enter phone: ");
-		String phone = scan.nextLine();
-		
-		if (!isPhoneNumber(phone)) 
-			continue;
-		if (!phoneBook.containsKey(name)) { 
-		phoneBook.put(name, new LinkedList<String>);
-		} else {
-			phoneBook.get(name).add(phone);
-			String name = scan.nextLine();
-		}
-	}
+    public static void main(String[] args) {
+        Map<String, List<String>> phoneBook = new HashMap<>();
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter name or \"quit\" for exit: ");
+            String name = scan.nextLine();  // запрос ввода имени (ключ)
+            if (name.equals("quit")) break; // проверка команды для "выход"
+
+            String phone = "";
+            do {
+                System.out.print("Enter phone (format numbers example: 89876543210\n>>>");
+                phone = scan.nextLine();    // запрос ввода номера телефона (значение)
+            }
+            while (!isPhoneNumber(phone));  // проверка корректности номера телефона
+
+            if (!phoneBook.containsKey(name)) {     // проверка присутствия ключа в словаре
+                phoneBook.put(name, new ArrayList<>()); // добавляем новое ключ/значение в словарь
+                phoneBook.get(name).add(phone); // добавляем номер телефона в значение, которое содержит список
+            } else {
+                phoneBook.get(name).add(phone); // если ключ уже есть в словаре, добавляем значение в список, который находится по указанному ключу
+            }
+        }
+
+        System.out.println(phoneBook);
     }
 }
 
--- homeworks/homework5/Task2.java
+++ homeworks/homework5/Task2.java
@@ -1,13 +1,58 @@
+package homework5;
+
+import java.util.*;
+
 /*
 Пусть дан список сотрудников: Иван, Пётр, Антон и так далее. 
 Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений. 
 Отсортировать по убыванию популярности.
 */
-public class Task2{
+public class Task2 {
 
-    public static void main(String args[]){
+    private static Map<String, Integer> employeesDuplicates(List<String> list) {
+        Map<String, Integer> map = new HashMap<>();
 
-        
+        for (String name : list) {
+            if (!map.containsKey(name)) {
+                map.put(name, 1);
+            } else {
+                int i = map.get(name);
+                map.put(name, i + 1);
+            }
+        }
+        return map;
+    }
+
+    private static Map<String, Integer> sortedEmployeesByValue(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue(((o1, o2) -> o2 - o1)));
+
+        Map<String, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        List<String> employees = new ArrayList<>();
+        employees.add("Ivan");
+        employees.add("Ivan");
+        employees.add("Denis");
+        employees.add("Masha");
+        employees.add("Alex");
+        employees.add("Denis");
+        employees.add("Denis");
+        employees.add("Ivan");
+        employees.add("Misha");
+        employees.add("Misha");
+        employees.add("Alex");
+        employees.add("Ivan");
+
+        Map<String, Integer> countingEmployees = sortedEmployeesByValue(employeesDuplicates(employees));
+        System.out.println(countingEmployees);
     }
 }
 
